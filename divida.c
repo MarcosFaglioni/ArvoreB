22e577eb501dd22638972d303a34fe27f265ea43
Leonardo Utida <leoutida@hotmail.com>
2017-01-29 02:42:38 -0200

    Create divida.c

---
+++ divida.c
@@ -0,0 +1,31 @@
+void divida(No* atual, float novaChave, No* dir, No* novo, float* promovida){
+    criaNoFolha(atual->ordem, &novo);
+    novo->tipo = atual->tipo;
+    int m;
+    m = floor((atual->ordem)/2);
+    if(novaChave < atual->elementos[m]){
+        int i;
+        int pos = 0;
+        for(i = m; i<atual->nElementos; i++){
+            novo->elementos[pos] = atual->elementos[m];
+            novo->filhos[pos+1] = atual->filhos[m+1];
+            novo->nElementos++;
+            atual->nElementos--;
+            pos++;
+        }
+        insere(&atual, novaChave);
+    }else{
+        int i;
+        int pos = 0;
+        for(i = m+1; i<atual->nElementos; i++){
+            novo->elementos[pos] = atual->elementos[m];
+            novo->filhos[pos+1] = atual->filhos[m+1];
+            novo->nElementos++;
+            atual->nElementos--;
+            pos++;
+        }
+        insere(&novo, novaChave);
+    }
+
+
+}
