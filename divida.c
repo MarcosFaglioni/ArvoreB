da6700381c71d06b3656520d572bf25d0f9dc6fb
Leonardo Utida <leoutida@hotmail.com>
2017-01-29 12:40:07 -0200

    Update divida.c

--- divida.c
+++ divida.c
@@ -1,15 +1,17 @@
 //COLOCA ISSO NA MAIN PRA TESTAR
     No testeS, novo;
-    criaNoFolha(4, &testeS);
-    insere(&testeS, 1);
+    criaNoFolha(7, &testeS);
     insere(&testeS, 2);
     insere(&testeS, 3);
+    insere(&testeS, 4);
+    insere(&testeS, 5);
+    insere(&testeS, 6);
+    insere(&testeS, 7);
+    int i;
     float promovida;
 
-    divida(&testeS, 4, &novo, &promovida);
-    printf("Elemento 0: %f\n", novo.elementos[0]);
+    divida(&testeS, 1, &novo, &promovida);
     printf("promovida: %f\n", promovida);
-    int i;
     for(i = 0; i<testeS.nElementos; i++){
         printf("Elemento de testeS: %f\n", testeS.elementos[i]);
     }
@@ -33,23 +35,28 @@ void divida(No* atual, float novaChave, No* novo, float* promovida){
     printf("valor: %f\n", atual->elementos[m]);
     printf("valor: %f\n",novaChave);
     if(novaChave < atual->elementos[m]){
+        printf("Entrou no IF\n");
         int i;
         int pos = 0;
-        for(i = m; i<atual->nElementos; i++){
-            novo->elementos[pos] = atual->elementos[m];
-            novo->filhos[pos+1] = atual->filhos[m+1];
+        int fim = atual->nElementos;
+        for(i = m; i<fim; i++){
+            novo->elementos[pos] = atual->elementos[i];
+            novo->filhos[pos+1] = atual->filhos[i+1];
             novo->nElementos++;
             atual->nElementos--;
             pos++;
         }
-        insere(&atual, novaChave);
+        printf("Saiu do FOR\n");
+        insere(atual, novaChave);
+        printf("Inseriu o valor\n");
     }else{
         printf("Entrou no ELSE\n");
         int i;
         int pos = 0;
-        for(i = m+1; i<atual->nElementos; i++){
-            novo->elementos[pos] = atual->elementos[m];
-            novo->filhos[pos+1] = atual->filhos[m+1];
+        int fim = atual->nElementos;
+        for(i = m+1; i<fim; i++){
+            novo->elementos[pos] = atual->elementos[i];
+            novo->filhos[pos+1] = atual->filhos[i+1];
             novo->nElementos++;
             atual->nElementos--;
             pos++;
